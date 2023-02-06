24a64bca48e0d6a9a4b85d72c0027ae783e41fb4
Or Rappel-Kroyzer <orkroyzer@gmail.com>
2023-02-06 21:43:40 +0200

    Code v1

---
+++ create_db.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import sys
+from pathlib import Path
+local_python_path = str(Path(__file__).parents[0])
+if local_python_path not in sys.path:
+   sys.path.append(local_python_path)
+from utils.utils import load_config, get_logger
+logger = get_logger(__name__)
+config = load_config(config_path=Path(local_python_path) / "config.json")
+import plotly.express as px
+from utils.plotly_utils import fix_and_write, get_colors
+import numpy as np
+import plotly.graph_objects as go
+import re
+import plotly
+categories = ['יו״ר הוועדה', 'יו״ר וועדה חלופי', 'שר המשפטים', 'ח״כ הקואליציה', 'מומחה תומך', 'ח״כ האופוזיציה', 'מומחה מתנגד',
+              'מנהל הועדה', 'יועמ"ש הכנסת', 'יועמ״ש הוועדה', 'עובד ציבור',  'דובר לא ידוע']
+
+force=True
+
+def analyze_speakers():
+    logger.info("analyzing by speaker")
+    tagging =pd.read_excel(Path(config['input_dir'])  / "speaker tagging.xlsx").set_index('שם דובר')['קטגוריה']
+    fn = Path(config['input_dir']) / 'speaker_in_meeting_by_chairperson.xlsx'
+    d = Path(config['input_dir']) / "processed_by_chairperson"
+    if not fn.exists() or force:
+        def ananlyze_by_speaker_and_chairman(x):
+            meeting_date = x.stem
+            df = pd.read_excel(x)
+            df['length'] = df.text.apply(lambda x: len(x.split()))
+            df = df.join(df.groupby('chairman')['length'].sum().rename('total_length'), on='chairman')
+            t1 = df.groupby(['name', 'chairman'])\
+                .agg({'length' : 'sum', 'total_length' : 'first'})\
+                    .reset_index()\
+                        .assign(meeting_date=meeting_date)
+            t1['אחוז השתתפות בישיבה לפי חתך יו"ר'] = 100*t1['length']/t1['total_length']
+            t1 = t1.drop(columns='total_length').rename(columns={'length' : "מס' מלים לפי חתך יור"})                         
+            return t1
+        df = pd.concat([ananlyze_by_speaker_and_chairman(x) for x in d.glob("*.xlsx")])\
+            .rename(columns={'name' : 'שם דובר', 'chairman' : 'יו"ר', 'meeting_date' : 'תאריך הישיבה'})\
+                .merge(tagging, on='שם דובר', how='left')
+        df.reset_index(inplace=True)   
+        logger.info(f"writing to {fn}")
+        df.to_excel(fn, index=False)
+
+    fn = Path(config['input_dir']) / 'speaker_in_meeting.xlsx'
+    if not fn.exists() or force:
+        def ananlyze_by_speaker(x):
+            meeting_date = x.stem
+            df = pd.read_excel(x)
+            df['length'] = df.text.apply(lambda x: len(x.split()))
+            df['אורך קטע לא מופרע מקסימלי'] = df['length']
+            df['ממוצע אורך קטע לא מופרע'] = df['length']
+            t2 = df.groupby('name')\
+                .agg({'length' : 'sum', 'אורך קטע לא מופרע מקסימלי' : 'max', 'ממוצע אורך קטע לא מופרע' : 'mean'})\
+                    .reset_index()\
+                        .assign(meeting_date=meeting_date)
+            t2['אחוז השתתפות בישיבה'] = 100*t2['length']/t2.length.sum()
+            t2 = t2.rename(columns={'length' : "מס' מלים"})                         
+            return t2
+        df = pd.concat([ananlyze_by_speaker(x) for x in d.glob("*.xlsx")])\
+            .rename(columns={'name' : 'שם דובר', 'chairman' : 'יו"ר', 'meeting_date' : 'תאריך הישיבה'})\
+                .merge(tagging, on='שם דובר', how='left')
+        df.reset_index(inplace=True) 
+        df['שם דובר'] = df['שם דובר'].apply(lambda x: x.split(" (")[0])
+        logger.info(f"writing to {fn}")
+        df.to_excel(fn, index=False)
+        
+        
+    return
+
+def split_by_chairperson():
+    logger.info("extracting chairperson")
+    source_dir = Path(config['input_dir']) / "processed"
+    target_dir = Path(config['input_dir']) / "processed_by_chairperson"
+    target_dir.mkdir(parents=True, exist_ok=True)
+    for x in source_dir.glob("*.xlsx"):
+        name = x.stem
+        if  (target_dir /f'{name}.xlsx').exists() and not force:
+            continue
+        df = pd.read_excel(x)
+        t = df.loc[df['name'].str.startswith("היו\"ר"), 'name']
+        df['chairman_segment'] = (t.shift(1, fill_value=t.head(1)) != t).cumsum()
+        df['chairman_segment'] = df['chairman_segment'].ffill()
+        df['chairman'] = df['chairman_segment'].replace(df.groupby('chairman_segment')['name'].first().to_dict())
+        df.to_excel(target_dir /f'{name}.xlsx')
+
+def process_formal_protocol():
+    logger.info("porocessing formal protocol")
+    speaker_pat = re.compile(r'<< \w+ >> (.+?): << \w+ >>', re.UNICODE|re.MULTILINE) # for files=7? check if regex works better
+    folder = Path(config['input_dir']) / 'formal_protocol'
+    target_folder = Path(config['input_dir']) / 'processed'
+    for filename in folder.glob('*.txt'):
+        logger.info(f"working on {filename}")
+        name = filename.stem
+        if  (target_folder /f'{name}.xlsx').exists() and not force:
+            continue
+        else:
+            text_file = open(filename, "r", encoding= 'utf8')
+            txt = text_file.read()
+            text_file.close() 
+            speaker_text = re.split(speaker_pat, txt)
+            speakers = []
+            texts = []
+            for i in range(1, int(len(speaker_text)/2)+1):
+                texts.append(speaker_text[i*2])
+                speakers.append(speaker_text[i*2-1])
+            df = pd.DataFrame(data={'file':name,'name':speakers,'text':texts})
+            df.text = df.text.apply(lambda x: x.replace("\n",''))
+            df.text = df.text.apply(lambda x: x.replace("-",''))
+            logger.info(f"writing to {target_folder} / {name}.xlsx'")
+            df.to_excel(target_folder /f'{name}.xlsx')
+
+def process_whisper():
+    speaker_pat = re.compile(r"([^\n]+)\n([^\n]+)\n\n+", re.UNICODE|re.MULTILINE)
+
+def main():
+    process_formal_protocol()
+    split_by_chairperson()
+    analyze_speakers()
+
+if __name__ == "__main__":             
+    main()
--- process.py
+++ process.py
@@ -8,108 +8,210 @@ from utils.utils import load_config, get_logger
 logger = get_logger(__name__)
 config = load_config(config_path=Path(local_python_path) / "config.json")
 import plotly.express as px
-from utils.plotly_utils import fix_and_write
+from utils.plotly_utils import fix_and_write, get_colors
+import numpy as np
+import plotly.graph_objects as go
+import re
+import plotly
+categories = ['יו״ר הוועדה', 'יו״ר וועדה חלופי', 'שר המשפטים', 'ח״כ הקואליציה', 'מומחה תומך', 'ח״כ האופוזיציה', 'מומחה מתנגד',
+              'מנהל הועדה', 'יועמ"ש הכנסת', 'יועמ״ש הוועדה', 'עובד ציבור',  'דובר לא ידוע']
+colors = ['rgb(253,50,22)', 
+             'rgb(0,254,53)', 
+             'rgb(106,118,252)', 
+             'rgb(254,212,196)', 
+             'rgb(254,0,206)', 
+             'rgb(13,249,255)', 
+             'rgb(246,249,38)', 
+             'rgb(255,150,22)', 
+             'rgb(71,155,85)', 
+             'rgb(238,166,251)', 
+             'rgb(220,88,125)', 
+             'rgb(214,38,255)']
 
-def by_category_comparison():    
+def p(l,t):
+    return [l[i-1] for i in t]
+
+
+
+def by_category_comparison(df):    
     logger.info("runnign conmapison by category")
-    df1 = pd.read_excel(Path(config['input_dir']) / "protocols by order updated timestamp.xlsx")
-    df1['תאריך הישיבה'] = '22.1.23'
-    df = pd.read_excel(Path(config['input_dir']) / "protocols older meetings.xlsx")
-    df = pd.concat([df, df1])
-    df.index = range(len(df))
-
-    tagging = pd.read_excel(Path(config['input_dir'])  / "speaker tagging.xlsx")
-    tagging1 = pd.read_excel(Path(config['input_dir'])  / "speaker tagging.xlsx", sheet_name='22.1').rename(columns={'דובר' : 'שם דובר'})
-    tagging = pd.concat([tagging.reset_index(drop=True), tagging1.reset_index(drop=True)])
-    tagging = tagging.groupby('שם דובר')['קטגוריה'].first()
-    tagging['אריאל קלנר'] = 'ח״כ הקואליציה'
-    tagging['טלי גוטליב'] = 'ח״כ הקואליציה'
-    tagging['יונתן מישרקי'] = 'ח״כ הקואליציה'
-    tagging['משה סעדה'] = 'ח״כ הקואליציה'
-    tagging['עמית הלוי'] = 'ח״כ הקואליציה'
-    tagging['?'] = 'דובר לא ידוע'
-    df = df.merge(tagging, on='שם דובר', how='left')
-    t = df.groupby(['קטגוריה', 'ישיבות'])["מס' מלים"].sum().reset_index()
-    t = t.rename(columns={'ישיבות' : 'ימי דיונים'})
-    t['ימי דיונים'] = t['ימי דיונים'].replace({'11-16.1' : '11-18.1'})
-    t.loc[9, "מס' מלים"] += 46
-    t.drop(6)
-    t = t.drop(6)
-    fig = px.pie(t, values="מס' מלים",  names="קטגוריה", title="אחוז ההשתפות לפי קטגוריה, 11-23.1.2023", facet_col='ימי דיונים', facet_col_spacing=0.2)
-    fig.update_layout(title={'xanchor': 'center', 'x':0.5})
+    
+    df.loc[~df['תאריך הישיבה'].isin(['22.1.23', '23.1.23', '30.1.23']), 'ימי דיונים'] = '11-18.1'
+    df.loc[df['תאריך הישיבה'].isin(['22.1.23', '23.1.23', '30.1.23']), 'ימי דיונים'] = '22-23.1'
+    df1 = df.assign(**{'ימי דיונים' : 'כל הישיבות'})
+    fig = px.pie(pd.concat([df, df1]).reset_index(), 
+                 values="מס' מלים",  
+                 names="קטגוריה", 
+                 title="אחוז ההשתפות לפי קטגוריה, 11-23.1.2023", 
+                 facet_col='ימי דיונים', 
+                 facet_col_spacing=0.15,
+                 color_discrete_sequence=colors[::-1],
+                 category_orders={'קטגוריה' : categories[::-1]})
+    fig.update_traces(textfont_size=25)
+    fig.update_layout(legend={'traceorder' : 'reversed',
+                              'font' : {'size' : 25},
+                              'orientation' : "h",
+                              'entrywidth' : 0.2, # change it to 0.3
+                              'entrywidthmode' : 'fraction',
+                              'xanchor' : 'center', 'yanchor' : 'bottom', 'x' : 0.5, 'y' : 0},
+                      title_font={'size' : 25})
     fix_and_write(fig=fig,
-                    filename="by_chairman_older_protocols_decending_order.png",
+                    filename="by_categories_comparison",
                     output_dir=config['output_dir'],
-                    height_factor=1/2,
-                    width_factor=1/2)
+                    height_factor=0.75,
+                    width_factor=0.9)
 
 def chairpersons_comparison():
     logger.info("runnign chairperson graph")
-    d = Path(config['input_dir']) / "older meetings"
+    d = Path(config['input_dir']) / "processed_by_chairperson"
     def foo(x):
-        meeting_date = x.stem[:-10]
-        df = df = pd.read_excel(x)
+        meeting_date = x.stem
+        df = pd.read_excel(x)
         df['length'] = df.text.apply(lambda x: len(x.split()))
         return pd.DataFrame(df.groupby('chairman').apply(lambda g: 100*g.loc[g['name'] == g['chairman'], 'length'].sum()/g.length.sum())\
                 .rename('אחוז מסך המלים בפרוטוקול'))\
                     .reset_index()\
                         .assign(meeting_date=meeting_date)
-    by_chairman = pd.concat([foo(x) for x in d.glob("*_processed.xlsx")])
+    by_chairman = pd.concat([foo(x) for x in d.glob("*.xlsx")])
     by_chairman['meeting_date'] = by_chairman['meeting_date'].str.replace('afternoon', 'אחה"צ')
     by_chairman = by_chairman.rename(columns={'meeting_date' : 'תאריך הישיבה'})
     by_chairman['יו"ר, תאריך הישיבה'] = by_chairman['chairman'] + ", " + by_chairman['תאריך הישיבה']
     by_chairman = by_chairman.rename(columns={'chairman' : 'יו"ר'})
+    by_chairman['day'] = by_chairman['תאריך הישיבה'].apply(lambda x: int(x.split(".")[0]))
     t = by_chairman.sort_values('אחוז מסך המלים בפרוטוקול', ascending=True)
-    fig = px.bar(t, y='יו"ר, תאריך הישיבה', x='אחוז מסך המלים בפרוטוקול', title=f'שיעור השתתפות של היו"ר (בחלק הישיבה בה כיהן כיו"ר) בפרוטוקול ועדת חוקה', orientation='h',  category_orders={'index': t.index[::-1]}, color='יו"ר')
-    fig.update_layout( yaxis={'categoryorder':'array', 'categoryarray':t['יו"ר, תאריך הישיבה']})
-    fig.update_layout(title={'xanchor': 'center', 'x':0.5})
+    fig = px.bar(t, y='יו"ר, תאריך הישיבה', x='אחוז מסך המלים בפרוטוקול', 
+                 title=f'שיעור השתתפות של היו"ר (בחלק הישיבה בה כיהן כיו"ר) בפרוטוקול ועדת חוקה', 
+                 orientation='h',  
+                 category_orders={'index': t.index[::-1]}, 
+                 color='יו"ר')
+    fig.update_layout( yaxis={'categoryorder':'array', 'categoryarray':t['יו"ר, תאריך הישיבה']},
+                        title={'xanchor': 'center', 'x':0.5},
+                        legend={'traceorder' : 'reversed', 'xanchor' : 'right', 'yanchor' : 'top', 'y' : 0.8})
     fix_and_write(fig=fig,
-                    filename="by_chairman_older_protocols_decending_order.png",
+                    filename="by_chairman_older_protocols_decending_order",
                     output_dir=config['output_dir'],
-                    height_factor=1/2,
-                    width_factor=1/2)
+                    height_factor=0.8,
+                    width_factor=0.8)
 
-def split_chairperson():
-    logger.info("extracting chairperson")
-    d = Path(config['input_dir']) / "older meetings"
-    for x in d.glob("*.xlsx"):
-        if str(x).endswith("_processed.xlsx"):
-            continue
-        df = pd.read_excel(x)
-        t = df.loc[df['name'].str.startswith("היו\"ר"), 'name']
-        df['chairman_segment'] = (t.shift(1, fill_value=t.head(1)) != t).cumsum()
-        df['chairman_segment'] = df['chairman_segment'].ffill()
-        df['chairman'] = df['chairman_segment'].replace(df.groupby('chairman_segment')['name'].first().to_dict())
-        df.to_excel(f"{str(x)[:-5]}_processed.xlsx")
-    df1 = pd.read_excel(Path(config['input_dir']) / "protocols by order updated timestamp.xlsx")
-    t = df1.loc[df1['name'].str.startswith("היו\"ר"), 'name']
-    df1['chairman_segment'] = (t.shift(1, fill_value=t.head(1)) != t).cumsum()
-    df1['chairman_segment'] = df1['chairman_segment'].ffill()
-    df1['chairman'] = df1['chairman_segment'].replace(df1.groupby('chairman_segment')['name'].first().to_dict())
-    d.mkdir(parents=True, exist_ok=True)
-    df1.to_excel(d / "22.1.23_processed.xlsx")
+
  
-def analyze_speakers():
-    logger.info("analyzing by speaker")
+def speaker_comparison(df):
+    for meeting_date in df['תאריך הישיבה'].unique():
+        fig = px.bar(df[df['תאריך הישיבה'] == meeting_date].sort_values('אחוז השתתפות בישיבה', ascending=False), 
+                     y='שם דובר', 
+                     x='אחוז השתתפות בישיבה', 
+                     title=f'שיעור השתתפות של דוברים שונים בפרוטוקול ועדת חוקה {meeting_date}', 
+                     orientation='h', 
+                     color='קטגוריה',
+                     color_discrete_sequence=colors[::-1],
+                     category_orders={'קטגוריה' : categories[::-1]})
+        fig.update_layout(legend={'traceorder' : 'reversed', 
+                                  'xanchor' : 'right', 'yanchor' : 'top', 'y' : 0.93})
+        (config['output_dir'] / "by_meeting" / "num_minutes").mkdir(parents=True, exist_ok=True)
+        
+        fix_and_write(fig=fig,
+                    filename=meeting_date,
+                    height_factor=1.5,
+                    output_dir=config['output_dir'] / "by_meeting" / "num_minutes")
+
+        fig = px.bar(df[df['תאריך הישיבה'] == meeting_date].sort_values('אורך קטע לא מופרע מקסימלי', ascending=False), 
+                     y='שם דובר', 
+                     x='אורך קטע לא מופרע מקסימלי', 
+                     title=f' אורך מקסימלי של קטע דיבור לא מופרע למשתתף{meeting_date}', 
+                     orientation='h', 
+                     color='קטגוריה',
+                     color_discrete_sequence=colors[::-1],
+                     category_orders={'קטגוריה' : categories[::-1]})
+        fig.update_layout(legend={'traceorder' : 'reversed', 
+                                  'xanchor' : 'right', 'yanchor' : 'top', 'y' : 0.93})
+        (config['output_dir'] / "by_meeting" / "max_segment").mkdir(parents=True, exist_ok=True)
+        fix_and_write(fig=fig,
+                    filename=meeting_date,
+                    height_factor=1.5,
+                    output_dir=config['output_dir'] / "by_meeting" / "max_segment")
+        fig = px.bar(df[df['תאריך הישיבה'] == meeting_date].sort_values('ממוצע אורך קטע לא מופרע', ascending=False), 
+                     y='שם דובר', 
+                     x='אורך קטע לא מופרע מקסימלי', 
+                     title=f' אורך ממוצע של קטע דיבור לא מופרע למשתתף{meeting_date}', 
+                     orientation='h', 
+                     color='קטגוריה',
+                     color_discrete_sequence=colors[::-1],
+                     category_orders={'קטגוריה' : categories[::-1]})
+        fig.update_layout(legend={'traceorder' : 'reversed', 
+                                  'xanchor' : 'right', 'yanchor' : 'top', 'y' : 0.93})
+        (config['output_dir'] / "by_meeting" / "mean_segment").mkdir(parents=True, exist_ok=True)
+        fix_and_write(fig=fig,
+                    filename=f"meeting_date",
+                    height_factor=1.5,
+                    output_dir=config['output_dir'] / "by_meeting" / "mean_segment")
+
+
+
+    return df
+
+def create_histogram_with_outliers(series, name, end, range=None):
+    start = int(series.min())
+    if range is None:
+        size = 5
+        # Making a histogram
+        largest_value = series.max()
+        if largest_value > end:
+            hist = np.histogram(series, bins=list(range(start, end+size, size)) + [largest_value], weights=series)
+        else:
+            hist = np.histogram(series, bins=list(range(start, end+size, size)) + [end+size], weights=series)
+    else:
+        hist = np.histogram(series, bins=range, weights=series)
+
+    # Adding labels to the chart
+    labels = []
+    for i, j in zip(hist[1][0::1], hist[1][1::1]):
+        if j <= end:
+            labels.append('{} - {}'.format(i, j))
+        else:
+            labels.append('> {}'.format(i))
+
+    # Plotting the graph
+    return go.Bar(x=labels,  y=hist[0], name=name)
+
+def word_seg_histograms():
     d = Path(config['input_dir']) / "older meetings"
-    def foo(x):
-        meeting_date = x.stem[:-10]
-        df = pd.read_excel(x)
-        df['length'] = df.text.apply(lambda x: len(x.split()))
-        df = df.join(df.groupby('chairman')['length'].sum().rename('total_length'), on='chairman')
-        df = df.groupby(['name', 'chairman'])\
-            .agg({'length' : 'sum', 'total_length' : 'first'})\
-                .reset_index()\
-                    .assign(meeting_date=meeting_date)\
-                        .rename(columns={'name' : 'שם דובר', 'chairman' : 'יו"ר', 'meeting_date' : 'תאריך הישיבה'})         
-        df['אחוז השתתפות'] = 100*df['length']/df['total_length']
-        return df.rename(columns={'length' : "מס' מלים"}).drop(columns='total_length')
-    df = pd.concat([foo(x) for x in d.glob("*_processed.xlsx")])
-    fn = config['output_dir'] / 'speaker_in_meeting_by_chairperson.xlsx'
-    logger.info(f"writing to {fn}")
-    df.to_excel(fn)
+    df = pd.concat([pd.read_excel(x) for x in d.glob("*_processed.xlsx")]).reset_index()
+    df = df.drop(columns=[x for x in df.columns if x.startswith("Unnamed")])
+    df["מס' מלים"] = df.text.apply(lambda x: len(x.split()))
+
+    tagging =pd.read_excel(Path(config['input_dir'])  / "speaker tagging.xlsx").set_index('שם דובר')['קטגוריה']
+    df = df.rename(columns={'name' : 'שם דובר', 'chairman' : 'יו"ר', 'meeting_date' : 'תאריך הישיבה'})\
+            .merge(tagging, on='שם דובר', how='left')
+
+    data = [
+        create_histogram_with_outliers(df.loc[df['שם דובר'] == 'היו"ר שמחה רוטמן', "מס' מלים"], "שמחה רוטמן", 
+                                        end=250, range=[0,3,6,10,20,30, 40, 50, 100, 250]),
+        create_histogram_with_outliers(df.loc[df['קטגוריה'] == 'ח״כ האופוזיציה', "מס' מלים"], 'ח"כים מהאופוזיציה',
+                                        end=250, range=[0,3,6,10,20,30, 40, 50, 100, 250]),
+    ]
+    layout = go.Layout(
+        title="היסטוגרמת אורכי קטע הדיבור ללא הפרעה"
+    )
+    fig = go.Figure(data=data, layout=layout)
+    fix_and_write(fig=fig,
+                    filename="segs_histogram",
+                    #height_factor=1.5,
+                    output_dir=config['output_dir'])
+
+    return fig
+
+def read_db():
+    speaker_in_meeting_by_chairperson = pd.read_excel(Path(config['input_dir']) / 'speaker_in_meeting_by_chairperson.xlsx')
+    speaker_in_meeting = pd.read_excel(Path(config['input_dir']) / 'speaker_in_meeting.xlsx')
+    return speaker_in_meeting_by_chairperson, speaker_in_meeting
 
 def main():
-    split_chairperson()
+    
+    speaker_in_meeting_by_chairperson, speaker_in_meeting = read_db()
+    speaker_comparison(speaker_in_meeting)
+    by_category_comparison(speaker_in_meeting)
     chairpersons_comparison()
-    by_category_comparison()
+    
+
+if __name__ == "__main__":             
+    main()
